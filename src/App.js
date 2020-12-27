236ac13eed7f27e32dfad9d8e0b0992b76651be1
EseAlli <essealli.ea@gmail.com>
2020-12-27 22:38:40 +0100

    Ready to deploy- Added Api that generates a ton of random words

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Header from './components/Header';
+import Figure  from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from './components/Word';
+import Notification from './components/Notification';
+import Popup from './components/Popup';
+import {showNotification as show} from './helpers/helpers';
+
+// const words = ['application', 'programming', 'interface', 'wizard', 'unicorn', 'cakes', 'data'];
+
 
 function App() {
+  const [playable, setPlayable] = useState(true)
+  const [correctLetters, setCorrectLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+  const [words, setWords] = useState([]);
+  const [selectedWord, setSelectedWord]= useState('');
+
+  const fetchword = async () =>{
+    const {data} = await axios.get('https://random-word-api.herokuapp.com/word', {
+      params:{
+        number: 200
+      }
+    });
+    setWords(data)
+    setSelectedWord(data[Math.floor(Math.random() * words.length)])
+  }
+
+  useEffect(() => {
+    fetchword();   
+  }, [])
+
+  useEffect(() => {
+    const handleKeydown = event =>{
+      const {key, keyCode} = event;
+        if (playable && keyCode >= 65 && keyCode <= 90) {
+          const letter = key.toLowerCase();
+    
+          if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+             setCorrectLetters(correctLetters => [...correctLetters, letter])
+            } else {
+              show(setShowNotification)
+            }
+          } else {
+            if (!wrongLetters.includes(letter)) {
+              setWrongLetters(wrongLetters => [...wrongLetters, letter])
+            } else {
+              show(setShowNotification)
+            }
+          }
+        }
+      }
+
+      window.addEventListener('keydown', handleKeydown)
+
+      return () =>{
+        window.removeEventListener('keydown', handleKeydown)
+      }
+  }, [correctLetters, wrongLetters, playable, selectedWord])
+
+  function playAgain(){
+    setPlayable(true);
+    setCorrectLetters([]);
+    setWrongLetters([]);
+    fetchword()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters}/>
+        <WrongLetters 
+          wrongLetters={wrongLetters}/>
+        <Word 
+          selectedWord={selectedWord} 
+          correctLetters={correctLetters}/>
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} setPlayable={setPlayable} selectedWord={selectedWord ? selectedWord : "unicorn"} playAgain={playAgain}/>
+      <Notification showNotification={showNotification}/>
+    </>
   );
 }
 
